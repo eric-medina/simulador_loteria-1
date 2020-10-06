f38c1e3d8e63ea5adc8070c0ba101bbef8a19d8b
eric-medina <eric.medina.em09@gmail.com>
2020-10-05 21:46:30 -0400

    Inclusão do sweetAlert2 no código

--- script.js
+++ script.js
@@ -17,9 +17,9 @@ function sortearNumeros(){
 function addToList(num, pos){
     if(num.length == 2){
         if(numEsco.includes(num)){
-            alert("Número escolhido anteriormente. Digite outro número")
+            alert2("Erro", "Número escolhido anteriormente. Digite outro número")
         }else if(parseInt(num)>60){
-            alert("O números digitado não pode ser maior que 60")
+            alert2("Erro", "O números digitado não pode ser maior que 60")
         }else{
             numEsco[pos-1] = num
         }
@@ -30,7 +30,7 @@ function verificarAcertos(){
     sortearNumeros()
     let cont = 0
     if(numEsco.length != 6){
-        alert("A quantidade de números escolhidos é menor que 6.\n Digite 6 números de 01 a 60 com duas casas decimais")
+        alert2("Erro", "A quantidade de números escolhidos é menor que 6.\n Digite 6 números de 01 a 60 com duas casas decimais")
     }else{
         for(var i=0; i<6; i++){
             if(numSort.includes(parseInt(numEsco[i]))){
@@ -50,4 +50,35 @@ function printNumSort(){
         li.classList.add("result");
         document.getElementById("numSort").append(li);
     }
+}
+
+
+function alert2(title, text){
+  let timerInterval
+Swal.fire({
+  title: title,
+  html: text,
+  timer: 5000,
+  timerProgressBar: true,
+  willOpen: () => {
+    Swal.showLoading()
+    timerInterval = setInterval(() => {
+      const content = Swal.getContent()
+      if (content) {
+        const b = content.querySelector('b')
+        if (b) {
+          b.textContent = Swal.getTimerLeft()
+        }
+      }
+    }, 1000)
+  },
+  onClose: () => {
+    clearInterval(timerInterval)
+  }
+}).then((result) => {
+  /* Read more about handling dismissals below */
+  if (result.dismiss === Swal.DismissReason.timer) {
+    console.log('O alerta foi fechado')
+  }
+})
 }
